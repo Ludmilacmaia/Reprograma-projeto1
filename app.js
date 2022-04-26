6aa837cdddac1db5d111c4b064c9ad4b830729d0
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-26 11:19:17 -0300

    Função buscar livros

---
+++ app.js
@@ -0,0 +1,26 @@
+
+const read = require('readline-sync')
+const buscarLivros = require('./controllers/buscarLivros')
+
+const resposta = read.question(`
+########## Menu #########
+
+1 - CATEGORIA: Buscar livros por categoria
+2 - ORDENAR: Ordenar livros por quantidade de páginas
+3 - RECOMENDADOS: Buscar livros recomendados
+4 - LISTA DE DESEJOS: Busca livros não lidos
+5 - SAIR
+
+Digite um numero[1-5]:
+
+`)
+// console.log(resposta)
+
+switch (resposta){
+    case '1':
+        buscarLivros()
+        break
+    case '2':
+    default:
+        console.log('fim do algoritmo')
+}
