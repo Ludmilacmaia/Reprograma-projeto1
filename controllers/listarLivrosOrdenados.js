0cbedf8bcec04f54b410194eb20f9167b643509d
Ludmila Maia <ludmilacmaia@gmail.com>
2022-04-27 12:09:06 -0300

    Modifica a função listar livros ordenados

--- controllers/listarLivrosOrdenados.js
+++ controllers/listarLivrosOrdenados.js
@@ -2,28 +2,23 @@ const livros = require('../database')
 const read = require('readline-sync')
 
 const listarLivrosOrdenados = () => {
-    const opcaoInicial = read.question('Deseja buscar por quantidade de páginas? S/N').toUpperCase()
+
+    const opcaoInicial = read.question('Deseja buscar por quantidade de paginas? S/N').toUpperCase()
 
     if (opcaoInicial === 'S'){
-        //todas os livros serão mostrados por ordem crescente de número de páginas
+
+        //todos os livros serão mostrados por ordem crescente de número de páginas
         const paginas = livros.map(livro => livro.paginas)
-        // console.table(paginas)
+    
         
         const paginasOrdenadas = paginas.sort((a,b) => a-b)
          console.table(paginasOrdenadas)
         
 
-        const inputPaginas = read.question('Digite o numero de paginas conforme a tabela acima')
-
-        const confirmacao = read.question('Tem certeza? S/N)').toUpperCase()
-
-        if(confirmacao ==='S'){
-            const livrosFiltrados = livros.filter(livro => livro.paginas === inputPaginas)
-            console.table(livrosFiltrados)
-            
+        
         }
     }
-}
+
 
 
 module.exports = listarLivrosOrdenados
